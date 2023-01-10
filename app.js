be5cd193133385bfb32b40282fa2d1b9845d6ce2
MQliferecord <17694822921@163.com>
2023-01-11 03:53:09 +0800

    notes更新

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ var logger = require('morgan');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
-//app.use(bodyParser.json())
+
 app.use(bodyParser.urlencoded({extended:true}));
+//app.use(bodyParser.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
+//app.use(express.json())
 app.use(cookieParser());
 //静态资源
 app.use(express.static(path.join(__dirname, 'public')));
--- constrollers/noteController.js
+++ constrollers/noteController.js
@@ -1,61 +1,94 @@
 let dbConf = require('../util/dbconfig')
-let NOTEMETHODS = require('./noteMethods')
 //增
 addNote = (req,res)=>{
-    let {content,dates,id} = req.body;
+    let {content,dates} = req.body;
     console.log(content)
-    NOTEMETHODS.addNote(content,dates,id).then((value)=>{
-        if(value == 'success'){
-            res.send({
-                'code':200,
-                'msg':'成功增加便签'
-            })
+    let sql = `insert into notes(content,dates) values(?,?)`
+    let sqlArr = [content,dates]
+    dbConf.sqlConnect(sql,sqlArr,(err,result)=>{
+        if(err){
+            throw new Error(err)
         }else{
-            res.send({
-                'code':400,
-                'msg':'增加便签失败'
-            })
-        }
-    })
-}
-//删
-deleteNote = (req,res)=>{
-    let id  = req.body.id;
-    NOTEMETHODS.deleteNote(id).then((value)=>{
-        if(value == 'success'){
-            res.send({
-                'code':200,
-                'msg':'成功删除便签'
-            })
-        }else{
-            res.send({
-                'code':400,
-                'msg':'删除便签失败'
-            })
+            console.log(result.insertId)
+            if(result.insertId){
+                let sqlans = `select * from notes where id=? `
+                let sqlansArr = [result.insertId]
+                dbConf.sqlConnect(sqlans,sqlansArr,(err,resans)=>{
+                    if(err){
+                        throw new Error(err)
+                    }else{
+                        if(resans.length){
+                            res.send({
+                                'code':200,
+                                'msg':'成功添加便签',
+                                'data':resans[0]
+                            })
+                        }
+                    }
+                })
+            }else{
+                res.send({
+                    'code':400,
+                    'msg':'添加便签失败'
+                })
+            }
         }
     })
     
 }
+//删
+deleteNote = (req,res)=>{
+    let id  = req.params.id;
+    console.log("delete"+id)
+    let sql = `delete from notes where id=?`
+    let sqlArr = [id]
+    dbConf.sqlConnect(sql,sqlArr,(err,result)=>{
+        if(err){
+            throw new Error(err)
+        }else{
+            if(result.affectedRows == 1){
+                res.send({
+                    'code':200,
+                    'msg':'成功删除便签'
+                })
+            }else{
+                res.send({
+                    'code':400,
+                    'msg':'删除便签失败',
+                })
+            } 
+        }
+    })
+}
 //改
 updateNote = (req,res)=>{
-    let {content,dates,id} = req.body
-    NOTEMETHODS.updateNote(content,dates,id).then((value)=>{
-        if(value == 'success'){
-            res.send({
-                'code':200,
-                'msg':'成功更新便签'
-            })
+    let {content,dates} = req.body;
+    let id = req.params.id
+    console.log(id)
+    console.log(content)
+    let sql = `update notes set content=?,dates=? where id=?`
+    let sqlArr = [content,dates,id]
+    dbConf.sqlConnect(sql,sqlArr,(err,result)=>{
+        if(err){
+            throw new Error(err)
         }else{
-            res.send({
-                'code':400,
-                'msg':'更新便签失败'
-            })
+            if(result.affectedRows == 1){
+                res.send({
+                    'code':200,
+                    'msg':'成功修改便签'
+                })
+            }else{
+                res.send({
+                    'code':400,
+                    'msg':'修改便签失败'
+                })
+            }
         }
     })
 }
 //根据内容模糊查询
 searchByContent = (req,res)=>{
-    let content = req.body.content
+    let content = req.params.content
     console.log(content)
     let sql = `select * from notes where content like ?`
     let sqlArr = ["%"+content+"%"]
@@ -81,8 +114,9 @@ searchByContent = (req,res)=>{
 }
 //根据id精确查询
 searchById = (req,res)=>{
-    let id = req.body.id
-    let sql = `select * from notes where uid=?`
+    let id = req.params.id
+    console.log("searchbyid"+id)
+    let sql = `select * from notes where id=?`
     let sqlArr = [id]
     dbConf.sqlConnect(sql,sqlArr,(err,result)=>{
         if(err){
@@ -107,7 +141,7 @@ searchById = (req,res)=>{
 
 //分页查询
 searchByPageSize = (req,res)=>{
-    let {page,size} = req.body
+    let {page,size} = req.params
     let currentPage = parseInt(page)||1
     let pageSize = parseInt(size)||15
     let offset = (currentPage-1)*pageSize;
